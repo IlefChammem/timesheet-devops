6a89b7277396e1db23f59bb41af356aff1832fd7
IlefChammem <ilef.chammem@esprit.tn>
2024-09-30 14:40:38 +0100

    modification 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -31,7 +31,7 @@ public class User implements Serializable {
 	
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
-	
+	//test!!!
 	@Enumerated(EnumType.STRING)
 	Role role; 
 	//Bonjour!!!
